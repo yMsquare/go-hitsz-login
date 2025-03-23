20c226c314368436d670c59e99a8b7823c68c4f0
msquare <akamsquare42@gmail.com>
2025-03-23 13:47:35 +0800

    assist

---
+++ src/assist/assist.go
@@ -0,0 +1,26 @@
+package assist
+
+import (
+	"context"
+	"log"
+
+	chromedp "github.com/chromedp/chromedp"
+)
+
+
+func ChromedpGetContent(url string ) string {
+	ctx, cancel := chromedp.NewContext(context.Background(),chromedp.WithLogf(log.Printf))
+	defer cancel()
+
+	var response string 
+	err := chromedp.Run(ctx, chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.OuterHTML("body",&response),
+	})
+
+	if err != nil{
+		log.Println(err)
+		return ""
+	}
+	return response
+}
